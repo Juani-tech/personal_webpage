7e9fc0b1957bb79e78bd32c431929c233e441def
JuaniFIUBA <jperezd@fi.uba.ar>
2026-02-05 19:54:56 +0000

    chore: remove ignored files from tracking

--- tests/seed.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test.describe('Theme Functionality Tests', () => {
-  test('seed', async ({ page }) => {
-    // Navigate to homepage
-    await page.goto('/');
-  });
-});
